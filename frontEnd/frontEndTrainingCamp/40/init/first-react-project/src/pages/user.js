ddc47a96b9e32e6e5f0e198f37ac3a3c1062a9dc
tdnq <2397105389@qq.com>
2022-03-07 22:34:22 +0800

    verify that the point of arrow function

--- frontEnd/frontEndTrainingCamp/40/init/first-react-project/src/pages/user.js
+++ frontEnd/frontEndTrainingCamp/40/init/first-react-project/src/pages/user.js
@@ -1,5 +1,25 @@
-export default function User(props){
-    return <div>
-        hello,{props.name}
-    </div>
+import React from "react";
+export default class Com extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'li s',
+            age: 12,
+        }
+        let self = this;
+        this.fn = {
+            changeName: {
+                fn: () => {
+                    this.setState((val) => { debugger; return { name: val.name + '1' } }); //class中 箭头函数的this总是指向class
+                }
+            },
+            changeName2: function () {
+                self.setState((val) => { debugger; return { name: val.name + '2' } });
+            }
+        }
+    }
+    render() {
+        console.log(this.state, 'state');
+        return <h1 onClick={() => { this.fn.changeName.fn(); this.fn.changeName2() }}>{this.state.name}</h1>
+    }
 }
