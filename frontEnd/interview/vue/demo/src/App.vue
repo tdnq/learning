8a34c096bf169a88914ee86365634a81005606f7
tdnq <2397105389@qq.com>
2022-03-07 23:55:50 +0800

    comunication between father and son

--- frontEnd/interview/vue/demo/src/App.vue
+++ frontEnd/interview/vue/demo/src/App.vue
@@ -1,18 +1,23 @@
+/* eslint-disable vue/no-unused-components */
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>
-    {{ n }}
-  </h1>
-  <HelloWorld isHiden msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld isHiden msg="Welcome to Your Vue.js App" /> -->
+  <ComunicationComponent />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import ComunicationComponent from "./components/Comulication.vue";
 
 export default {
   name: "App",
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     HelloWorld,
+    ComunicationComponent,
+  },
+  data() {
+    return { currentCom: "ComunicationComponent", comps: this.components };
   },
 };
 </script>
---
+++ frontEnd/interview/vue/demo/src/components/Comulication.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <h1>{{ name }}</h1> -->
+  <input v-model="name" type="text" />
+  <button @click="addHandler(title)">add</button>
+  <h2>events:</h2>
+  <ListItems @del="delHandler" :list="event" />
+</template>
+<script>
+import ListItems from "./List.vue";
+export default {
+  name: "ComunicationComponent",
+  components: {
+    ListItems,
+  },
+  data() {
+    return {
+      name: "dd",
+      event: ["test"],
+    };
+  },
+  methods: {
+    addHandler() {
+      console.log(this.event);
+      this.event.push(`${this.name}`);
+    },
+    delHandler(title) {
+      this.event = this.event.filter((item) => item !== title);
+    },
+  },
+};
+</script>
