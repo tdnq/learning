e947f5536a9dd66dcbc52dcd66da83aef288508f
tdnq <2397105389@qq.com>
2023-08-13 18:39:55 +0800

    Rate component

---
+++ frontEnd/geekbang/viteForVue/vue-project/src/components/Rate/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    value: { type: Number, default: 1 },
+    color: { type: String, default: 'red' }
+});
+let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value))
+</script>
+<template>
+    <div :style="{ color }">
+        {{ rate }}
+    </div>
+</template>
--- frontEnd/geekbang/viteForVue/vue-project/src/views/Vuex/index.vue
+++ frontEnd/geekbang/viteForVue/vue-project/src/views/Vuex/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-// const name = this.;
 import { useStore } from 'vuex';
+import Rate from '@/components/Rate/index.vue';
 const store = useStore();
 const changeAppName = () => {
     store.commit('app/change', 'faf')
@@ -9,4 +9,9 @@ const changeAppName = () => {
 <template>
     <h2>app name is : {{ $store.state.app.name }}</h2>
     <button @click="changeAppName">change</button>
+    <div>
+        <Rate :value="3" />
+        <Rate :value="2" color="blue" />
+    </div>
 </template>
+
